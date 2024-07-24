e4aa9d9c440950d0021edccdc205428f60f4f02a
Harshi1216 <harshithapaduchuri20@gmail.com>
2024-07-24 18:27:06 +0530

    Update EditCustomer.js

--- EditCustomer.js
+++ EditCustomer.js
@@ -8,4 +8,45 @@ const EditCustomer = () => {
     const history = useHistory();
     const dispatch = useDispatch();
     const customers = useSelector((state) => state.customers.customers);
-    const customer = customers.find((cust) => cust.id
+    const customer = customers.find((cust) => cust.id === parseInt(id));
+
+    const [name, setName] = useState(customer ? customer.name : '');
+    const [email, setEmail] = useState(customer ? customer.email : '');
+
+    useEffect(() => {
+        if (customer) {
+            setName(customer.name);
+            setEmail(customer.email);
+        }
+    }, [customer]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(editCustomer(id, { name, email }));
+        history.push('/');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Name:</label>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label>Email:</label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <button type="submit">Edit Customer</button>
+        </form>
+    );
+};
+
+export default EditCustomer;
