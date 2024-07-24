543e1e0baa75e7dcb1a13f3db1a3e084a13a512c
Harshi1216 <harshithapaduchuri20@gmail.com>
2024-07-24 18:26:20 +0530

    Update customerReducer.js

--- customerReducer.js
+++ customerReducer.js
@@ -1,1 +1,43 @@
+import {
+    FETCH_CUSTOMERS_REQUEST,
+    FETCH_CUSTOMERS_SUCCESS,
+    FETCH_CUSTOMERS_FAILURE,
+    ADD_CUSTOMER,
+    EDIT_CUSTOMER,
+    DELETE_CUSTOMER
+} from '../actions/customerActions';
 
+const initialState = {
+    customers: [],
+    loading: false,
+    error: null
+};
+
+const customerReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_CUSTOMERS_REQUEST:
+            return { ...state, loading: true };
+        case FETCH_CUSTOMERS_SUCCESS:
+            return { ...state, loading: false, customers: action.payload };
+        case FETCH_CUSTOMERS_FAILURE:
+            return { ...state, loading: false, error: action.error };
+        case ADD_CUSTOMER:
+            return { ...state, customers: [...state.customers, action.payload] };
+        case EDIT_CUSTOMER:
+            return {
+                ...state,
+                customers: state.customers.map(customer =>
+                    customer.id === action.payload.id ? action.payload : customer
+                )
+            };
+        case DELETE_CUSTOMER:
+            return {
+                ...state,
+                customers: state.customers.filter(customer => customer.id !== action.payload)
+            };
+        default:
+            return state;
+    }
+};
+
+export default customerReducer;
