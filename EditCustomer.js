d16e7c5df6846a21af06498c4c051e5c66cbeae3
Harshi1216 <harshithapaduchuri20@gmail.com>
2024-07-24 18:25:30 +0530

    Update EditCustomer.js

--- EditCustomer.js
+++ EditCustomer.js
@@ -1,1 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { editCustomer } from '../actions/customerActions';
+import { useParams, useHistory } from 'react-router-dom';
 
+const EditCustomer = () => {
+    const { id } = useParams();
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const customers = useSelector((state) => state.customers.customers);
+    const customer = customers.find((cust) => cust.id
